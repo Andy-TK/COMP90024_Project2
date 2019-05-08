1c6edf618f680ecedf0bed2700e60a1b56fae3d0
neetordy <jokecreater@gmail.com>
2019-05-09 01:11:09 +1000

    change templates a little bit 2

--- WebServer_Flask/app/__init__.py
+++ WebServer_Flask/app/__init__.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 # server = Server('http://115.146.95.246:5984')
 # db = server['twitter_rest']
 
+
+
 # import view_db.couchdb
-from app import model
+# from app import model
 from app import controller
