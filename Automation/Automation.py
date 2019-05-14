c1fc493bccc6d450c39bf521f13519e04098a566
neetordy <jokecreater@gmail.com>
2019-05-14 11:51:02 +1000

    change uwsgi setting

--- Automation/Automation.py
+++ Automation/Automation.py
@@ -1,13 +1,10 @@
 import os
 import pexpect
 import sys
+import time
 
 if __name__ == "__main__":
     process = pexpect.spawn('sh ./script_os.sh')
-    #                     events={
-    # 'Please enter your OpenStack Password for project unimelb-comp90024-group-35 as user haoyu.zhang@student.unimelb.edu.au:': 'YTljYzQzZTM1OTkwNjY4\n',
-    # 'BECOME password:': '9588\n'
-    # }, logfile=sys.stdout.buffer)
     process.logfile_read = sys.stdout.buffer
     process.expect(
         'Please enter your OpenStack Password for project unimelb-comp90024-group-35 as user haoyu.zhang@student.unimelb.edu.au:')
@@ -15,20 +12,16 @@ if __name__ == "__main__":
     i = process.expect('BECOME password:')
     process.sendline('9588')
     process.expect(pexpect.EOF)
-    # pattern = "\n(\S+).*?([0-9]+)%"
-    # filesystem_list = []
 
-    # for dummy in range(0, 100):
-    #     try:
-    #         i = process.expect('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-    #         if i == 0:
-    #             f = open("hosts", "a+")
-    #             # print(process.match.group(0))
-    #             string = str(process.match.group(0), encoding="utf-8")
-    #             f.write(string)
-    #             f.close()
-    #     except:
-    #         break
 
-    # pexpect.run
+    time.sleep(300)
+
+    pexpect.run('sh ./script_web.sh', logfile=sys.stdout.buffer)
+
+
+
+    # pexpect.run('sh ./script_os.sh', events={
+    #     'Please enter your OpenStack Password for project unimelb-comp90024-group-35 as user haoyu.zhang@student.unimelb.edu.au:': 'YTljYzQzZTM1OTkwNjY4\n',
+    #     'BECOME password:':'9588\n'
+    # }, logfile=sys.stdout.buffer)
 
