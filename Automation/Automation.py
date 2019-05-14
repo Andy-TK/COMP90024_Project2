eef7f6fa2a91758698dabfa362a476fa1465465e
neetordy <jokecreater@gmail.com>
2019-05-14 15:17:02 +1000

    add Harvest

--- Automation/Automation.py
+++ Automation/Automation.py
@@ -18,6 +18,11 @@ if __name__ == "__main__":
     process.sendline('9588')
     process.expect(pexpect.EOF)
 
+    # pexpect.run('sh ./script_os.sh', events={
+    #     'Please enter your OpenStack Password for project unimelb-comp90024-group-35 as user haoyu.zhang@student.unimelb.edu.au:': 'YTljYzQzZTM1OTkwNjY4\n',
+    #     'BECOME password:':'9588\n'
+    # }, logfile=sys.stdout.buffer)
+
     logging.info('Waiting for setting up instance for 2 minutes:')
     time.sleep(120)
 
