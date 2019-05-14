c9e318e98894963cd69c06865dcdb68dd04adf84
neetordy <jokecreater@gmail.com>
2019-05-14 21:33:25 +1000

    final

--- WebServer_Flask/app/__init__.py
+++ WebServer_Flask/app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from couchdb import Server
 
 app = Flask(__name__)
 # server = Server('http://115.146.95.246:5984')
 # db = server['twitter_rest']
 
+server = Server('http://172.26.38.7:5984')
+db = server['city_resultdb']
 
 
 # import view_db.couchdb
